a64ad7e569f291e0906e559c2d9c50ed06073182
Sebastian Losada <sebastian.losada.08@gmail.com>
2024-03-10 09:14:45 -0500

    fix(App): change AppBar because is used in other screens
    - only put the chat action on Dashboard appBar

--- app/lib/ui/components/custom_app_bar.dart
+++ app/lib/ui/components/custom_app_bar.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 AppBar customAppBar(String title, context) {
   return AppBar(
-    title: Center(
-      child: Text(
-          title,
-          style: GoogleFonts.nunito(
-            textStyle: const TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-            ),
+    centerTitle: true,
+    title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.nunito(
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
           ),
-      ),
+        ),
     ),
-    actions: [
-      IconButton(
-        onPressed: () => GoRouter.of(context).pushNamed('chats'),
-        icon: const Icon(Icons.chat_bubble_outline, color: Colors.white),
-      ),
-    ],
     backgroundColor: Colors.black,
   );
 }
