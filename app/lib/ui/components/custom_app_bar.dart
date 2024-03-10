af56a5a2964afc3b97488b9648e33aad72f2cf47
Sebastian Losada <sebastian.losada.08@gmail.com>
2024-03-10 09:06:20 -0500

    feat(App): add link to go chat
    - improve style of chat screen

--- app/lib/ui/components/custom_app_bar.dart
+++ app/lib/ui/components/custom_app_bar.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 AppBar customAppBar(String title, context) {
   return AppBar(
     title: Center(
       child: Text(
-        title,
-        style: GoogleFonts.nunito(
-          textStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+          title,
+          style: GoogleFonts.nunito(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
       ),
     ),
+    actions: [
+      IconButton(
+        onPressed: () => GoRouter.of(context).pushNamed('chats'),
+        icon: const Icon(Icons.chat_bubble_outline, color: Colors.white),
+      ),
+    ],
     backgroundColor: Colors.black,
   );
 }
